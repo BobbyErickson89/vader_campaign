8ef4c06d41942eafd6e007eae9f8e21eefaa81dc
Bobby Erickson <ericksonr07@gmail.com>
2017-06-19 19:44:01 -0400

    sign up form validation
    Completed sign-up form validation, along with new text that will be
    displayed when that’s completed.  Also did some clean-up of the css

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+//function making sure that we can only type numbers in the zipcode input
+$("#zipcode-input").keydown(function (e) {
+    // Allow: backspace, delete, tab, escape, and enter
+    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110]) !== -1 ||
+        // Allow: Ctrl+A, Command+A
+        (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+        // Allow: home, end, left, right, down, up
+        (e.keyCode >= 35 && e.keyCode <= 40)) {
+        // let it happen, don't do anything
+        return;
+    }
+    // Ensure that it is a number and stop the keypress
+    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+        e.preventDefault();
+    }
+});
+
 //Switching the arrow icon each time the 'Learn More About Darth' button is clicked.
 var changeArrows = function(){
     if($('#arrow-icon').hasClass('fa-angle-down')){
@@ -10,19 +27,14 @@ var changeArrows = function(){
     }
 };
 
+$('#learn-more-btn').click(changeArrows);
+
+//removing the sign up form from the page
+//this function is being called in the onSubmit attribute of #sign-up-form
 var completeSignUp = function(e){
     e.preventDefault();
     e.stopPropagation();
 
-    //removing the sign up form from the page
     $('#sign-up-form').remove();
-
-    //showing modal after successful sign-up completion
-    $(document).ready(function(){
-        $('#thankyouModal').modal('show');
-    });
-
+    $('#thank-you').fadeIn('slow');
 };
-
-$('#learn-more-btn').click(changeArrows);
-// $('#sign-up-btn').submit(completeSignUp);
